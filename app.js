3bd18c5e9175cdd18c21837be363a5726409ebd0
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2016-03-31 12:54:25 -0500

    Implement main game loop

--- app.js
+++ app.js
@@ -3,5 +3,33 @@
 var app = angular.module('ticTacToe', []);
 
 app.controller('gameCtrl', function($scope) {
-  $scope.answer = 42;
+  var whoseTurn = 'X';
+
+  $scope.newGame = function() {
+    whoseTurn = 'X';
+    $scope.turnIndicator = whoseTurn + "'s turn";
+    $scope.board = [['', '', ''], ['', '', ''], ['', '', '']];
+    $scope.gameRunning = true;
+  }
+
+  // immediately start a game
+  $scope.newGame();
+
+  $scope.handleClick = function(row, col) {
+    if ($scope.board[row][col] || !$scope.gameRunning) {
+      return;
+    }
+
+    $scope.board[row][col] = whoseTurn;    
+
+    if (checkForWin()) {
+      $scope.gameRunning = false;
+    } else {
+      whoseTurn = (whoseTurn === 'X') ? 'O' : 'X';
+    }
+  }
+
+  function checkForWin() {
+  }
+
 });
